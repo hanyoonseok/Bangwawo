77256fb904a7cadf4edbb446c57d0217b2bf7c71
한윤석 <hys6078@naver.com>
2022-07-24 15:51:43 +0900

    feat : init pjt

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HeaderNav />
+  </div>
+</template>
+
+<script>
+import HeaderNav from "./components/HeaderNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderNav,
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/HeaderNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "HeaderNav",
+};
+</script>
+
+<style></style>
