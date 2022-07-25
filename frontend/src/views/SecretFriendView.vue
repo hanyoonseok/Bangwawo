34088804f95d6975a18915aa1fbe8779f6956ce9
한윤석 <hys6078@naver.com>
2022-07-25 17:25:35 +0900

    etc : #3 secret friend 작성 전 체크포인트

---
+++ frontend/src/views/SecretFriendView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>home view</div>
+</template>
+
+<script>
+export default {
+  name: "SecretFriendView",
+};
+</script>
+
+<style scoped src="@/css/secretFriend.scss" lang="scss"></style>
