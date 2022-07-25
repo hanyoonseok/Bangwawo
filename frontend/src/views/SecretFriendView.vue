651ab3b8b35145e1d7cb2341b4051b541f4c5c16
한윤석 <hys6078@naver.com>
2022-07-25 17:44:28 +0900

    fix : global style app 수정

---
+++ frontend/src/views/SecretFriendView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="background"></div>
+</template>
+
+<script>
+export default {
+  name: "SecretFriendView",
+};
+</script>
+
+<style scoped src="@/css/secretFriend.scss" lang="scss"></style>
