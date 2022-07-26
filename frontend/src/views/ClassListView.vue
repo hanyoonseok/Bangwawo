ef1cc8ab3158f8a5b1f453a0c29181d284a37b88
Lee Hwayeon <snw4002@hanmail.net>
2022-07-26 16:27:16 +0900

    [FE] feat : #4 슬라이더 구현

--- frontend/src/views/ClassListView.vue
+++ frontend/src/views/ClassListView.vue
@@ -6,36 +6,39 @@
         <h2>공개 수업</h2>
         <p>현재 진행중인 공개수업 목록입니다.</p>
       </div>
-      <div class="contents slide">
-        <ul class="slides">
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-          <li>
-            <a><rect-post-card /></a>
-          </li>
-        </ul>
-      </div>
-      <div class="controller">
-        <span class="prev" @click="prev()"
-          ><i class="fa-solid fa-chevron-left"></i
-        ></span>
-        <span class="next" @click="next()"
-          ><i class="fa-solid fa-chevron-right"></i
-        ></span>
-      </div>
+      <swiper
+        :slidesPerView="3"
+        :slidesPerGroup="3"
+        :spaceBetween="10"
+        :loop="true"
+        :loopFillGroupWithBlank="true"
+        :navigation="true"
+        :style="{
+          '--swiper-navigation-color': '#C0C0C0',
+          '--swiper-pagination-color': '#C0C0C0',
+        }"
+        :modules="modules"
+        class="slide"
+      >
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+        <swiper-slide
+          ><a href=""><RectPostCard /></a
+        ></swiper-slide>
+      </swiper>
     </section>
     <section class="maked">
       <div class="search-bar">
@@ -70,6 +73,8 @@
           </li>
         </ul>
       </div>
+      <div class="swiper-button-prev">dd</div>
+      <div class="swiper-button-next">dd</div>
     </section>
     <a class="up" href="#top">
       <i class="fa-solid fa-arrow-up"></i>
@@ -79,60 +84,29 @@
 
 <script>
 import RectPostCard from "@/components/common/RectPostCard.vue";
+
+//vue-awesome-swiper 라이브러리
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper";
+
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 export default {
   name: "ClassListView",
   components: {
     RectPostCard,
+    Swiper,
+    SwiperSlide,
   },
-  data() {
-    return { currentIdx: 0 };
-  },
-  methods: {
-    next() {
-      const slides = document.querySelector(".slides"); //전체 슬라이드 컨테이너
-      const slideItem = document.querySelectorAll(".slides li"); //모든 슬라이드들
-      console.log(slideItem.length);
-      const slideCount = slideItem.length; // 슬라이드 개수
-      console.log(slideCount);
-      //   const next = document.querySelector(".next"); //다음 버튼
-      //   const slideWidth = 350; //한개의 슬라이드 넓이
-      //   const slideMargin = 32; //슬라이드간의 margin 값
-
-      //전체 슬라이드 컨테이너 넓이 설정
-      //   slides.style.width = (slideWidth + slideMargin) * slideCount + "px";
-
-      function moveSlide(num) {
-        slides.style.left = -num * 382 + "px";
-        this.currentIdx = num;
-      }
-
-      //   next.addEventListener("click", function () {
-      if (this.currentIdx !== slideCount - 1) {
-        moveSlide(this.currentIdx + 1);
-      }
-      //   });
-    },
-    prev() {
-      const slides = document.querySelector(".slides"); //전체 슬라이드 컨테이너
-      const slideItem = document.querySelectorAll(".slides li"); //모든 슬라이드들
-      console.log(slideItem.length);
-      const slideCount = slideItem.length; // 슬라이드 개수
-      console.log(slideCount);
-
-      //   const prev = document.querySelector(".prev"); //이전 버튼
-      //   const slideWidth = 350; //한개의 슬라이드 넓이
-      //   const slideMargin = 32; //슬라이드간의 margin 값
-
-      //전체 슬라이드 컨테이너 넓이 설정
-      //   slides.style.width = (slideWidth + slideMargin) * slideCount + "px";
-
-      function moveSlide(num) {
-        slides.style.left = -num * 382 + "px";
-        this.currentIdx = num;
-      }
-      console.log("??", this.currentIdx);
-      if (this.currentIdx !== 0) moveSlide(this.currentIdx - 1);
-    },
+  setup() {
+    return {
+      modules: [Navigation],
+    };
   },
 };
 </script>
