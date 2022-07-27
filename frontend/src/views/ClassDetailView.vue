81d3ffdb65b75eda4ff413210b005954ae2133f9
Lee Hwayeon <snw4002@hanmail.net>
2022-07-27 22:57:40 +0900

    [FE] fix : #7 강사 프로필 모달창 클릭 이벤트 수정

--- frontend/src/views/ClassDetailView.vue
+++ frontend/src/views/ClassDetailView.vue
@@ -6,6 +6,7 @@
         <i class="fa-solid fa-chevron-left"></i>
       </button>
       <div class="detail">
+        <div class="empty" @click="hideProfile"></div>
         <div class="img-box">
           <img src="@/assets/backhead.png" alt="오리 뒷모습" />
         </div>
@@ -121,10 +122,17 @@ export default {
       }
     };
 
+    const hideProfile = () => {
+      if (document.querySelector(".profile").style.display === "block") {
+        document.querySelector(".profile").style.display = "none";
+      }
+    };
+
     return {
       state,
       user,
       showProfile,
+      hideProfile,
     };
   },
 };
