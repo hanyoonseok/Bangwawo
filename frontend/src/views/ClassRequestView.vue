34da90d951430c25c1f4c26348bdb3d781e01830
Lee Hwayeon <snw4002@hanmail.net>
2022-07-29 21:28:17 +0900

    [FE] feat : #10 페이지네이션 UI 구현

--- frontend/src/views/ClassRequestView.vue
+++ frontend/src/views/ClassRequestView.vue
@@ -37,7 +37,7 @@
             <td>{{ item.num }}</td>
             <td>{{ item.writer }}</td>
             <td>
-              <router-link to="class/request/detail">
+              <router-link to="class/requestdetail">
                 {{ item.title }}
               </router-link>
             </td>
@@ -56,14 +56,30 @@
         </tbody>
       </table>
     </div>
-    <a class="up" @click="top">
-      <i class="fa-solid fa-arrow-up"></i>
-    </a>
+    <div class="pagination-wrapper">
+      <div v-if="page.startPage > 1">
+        <i class="fa-solid fa-angles-left"></i>
+      </div>
+      <div v-if="page.startPage > 1">
+        <i class="fa-solid fa-angle-left"></i>
+      </div>
+      <div class="page" v-for="(item, index) in page.pageRange" :key="index">
+        <router-link to="item">{{ item }}</router-link>
+      </div>
+      <div v-if="page.endPage < page.totalPage">
+        <i class="fa-solid fa-angle-right"></i>
+      </div>
+      <div v-if="page.endPage + 2 < page.totalPage">
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderNav from "@/components/HeaderNav.vue";
+import { computed, reactive } from "vue";
+
 export default {
   name: "ClassRequestView",
   components: {
@@ -136,136 +152,51 @@ export default {
         views: 1,
         want: 1,
       },
-      {
-        num: 1,
-        writer: "앵그리윤석",
-        title:
-          "앵그리윤석이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기진호",
-        title:
-          "애기진호는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: false,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기다빈",
-        title:
-          "애기다빈이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기수콩(김)",
-        title:
-          "애기수콩이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: false,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기화연",
-        title:
-          "애기화연이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기수콩(정)",
-        title:
-          "애기수콩이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "앵그리윤석",
-        title:
-          "앵그리윤석이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기진호",
-        title:
-          "애기진호는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: false,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기다빈",
-        title:
-          "애기다빈이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기수콩(김)",
-        title:
-          "애기수콩이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: false,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기화연",
-        title:
-          "애기화연이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
-      {
-        num: 1,
-        writer: "애기수콩(정)",
-        title:
-          "애기수콩이는 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다 이러이러한 수업을 듣고 싶슴다",
-        solved: true,
-        time: "14:55",
-        views: 1,
-        want: 1,
-      },
     ];
 
-    const top = () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    };
+    const page = reactive({
+      startPage: 2, //화면에 보이는 페이지 시작 번호
+      currentPage: 3, // 현재 페이지 번호
+      totalCount: 50, // 총 게시글 수
+      pageRange: computed(() => {
+        let array = [];
+        for (
+          let index = page.currentPage - 2;
+          index <= page.currentPage + 2;
+          index++
+        ) {
+          array.push(index);
+        }
+        return array;
+      }),
+      endPage: 6, // 화면에 보이는 페이지 끝 번호
+      totalPage: computed(() => {
+        return page.totalCount / 5;
+      }), // 총 페이지 수
+    });
+
+    // const pageRange = computed(() => {
+    //   let array = [];
+    //   for (
+    //     let index = page.currentPage - 2;
+    //     index <= page.currentPage + 2;
+    //     index++
+    //   ) {
+    //     array.push(index);
+    //   }
+    //   return array;
+    // });
+
+    // const totalPage = computed(() => {
+    //   return page.totalCount / 5;
+    // });
 
     return {
       user,
       requests,
-      top,
+      page,
+      // pageRange,
+      // totalPage,
     };
   },
 };
