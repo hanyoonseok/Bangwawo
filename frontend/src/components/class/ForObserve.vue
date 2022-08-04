68b6262cc7c5580caa8e39b57832cc41e162b1d1
한윤석 <hys6078@naver.com>
2022-08-04 16:56:41 +0900

    [FE] feat : #32 classlist 무한스크롤 기능 추가

---
+++ frontend/src/components/class/ForObserve.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="trigger">-</div>
+</template>
+<script>
+import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+
+export default defineComponent({
+  setup(props, { emit }) {
+    const trigger = ref();
+    const options = {
+      root: null,
+      threshold: 1,
+    };
+    let observer = null;
+    const handleIntersect = (entry) => {
+      if (entry.isIntersecting) emit("triggerIntersected", trigger.value);
+    };
+
+    onMounted(() => {
+      try {
+        observer = new IntersectionObserver((entries) => {
+          handleIntersect(entries[0]);
+        }, options);
+        observer.observe(trigger.value);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    onUnmounted(() => {
+      observer.value.disconnect();
+    });
+
+    return {
+      trigger,
+    };
+  },
+});
+</script>
