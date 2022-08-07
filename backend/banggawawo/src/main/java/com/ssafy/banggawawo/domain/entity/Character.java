1b884eb83394f13717c35de2dd1ed555492cb587
육다빈 <yukdabin@naver.com>
2022-08-07 11:19:40 +0900

    [BE] feat : #23 캐릭터 변수명 변경

--- backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Character.java
+++ backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Character.java
@@ -9,13 +9,13 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Character {
-    private Long ch_id;          //캐릭터 아이디        //전부 default 필요
-    private String ch_body;     //3d 캐릭터 몸 색상
-    private String ch_legs;     //3d 캐릭터 다리 색상
-    private String ch_glasses;  //3d 캐릭터 안경 색상
-    private String ch_mouse;    //3d 캐릭터 입 색상
-    private String ch_dress;    //3d 캐릭터 드레스 색상
-    private String ch_bag;      //3d 캐릭터 방 색상
+    private Long chId;          //캐릭터 아이디        //전부 default 필요
+    private String body;     //3d 캐릭터 몸 색상
+    private String legs;     //3d 캐릭터 다리 색상
+    private String glasses;  //3d 캐릭터 안경 색상
+    private String mouse;    //3d 캐릭터 입 색상
+    private String dress;    //3d 캐릭터 드레스 색상
+    private String bag;      //3d 캐릭터 방 색상
 
 }
 
