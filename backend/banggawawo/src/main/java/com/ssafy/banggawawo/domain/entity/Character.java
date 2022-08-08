c2a485fbe48b49bc64bb6318512f59c6b68b2ac7
육다빈 <yukdabin@naver.com>
2022-08-09 06:57:34 +0900

    [BE] feat : #23 이메일로 학생 계정찾기 추가

--- backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Character.java
+++ backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Character.java
@@ -9,14 +9,12 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Character {
-    private Long chId;          //캐릭터 아이디        //전부 default 필요
     private String body;     //3d 캐릭터 몸 색상
     private String legs;     //3d 캐릭터 다리 색상
     private String glasses;  //3d 캐릭터 안경 색상
-    private String mouse;    //3d 캐릭터 입 색상
     private String dress;    //3d 캐릭터 드레스 색상
     private String bag;      //3d 캐릭터 방 색상
-
+    private String hat;    //3d 캐릭터 모자 색상
 }
 
 
--- backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Volunteer.java
+++ backend/banggawawo/src/main/java/com/ssafy/banggawawo/domain/entity/Volunteer.java
@@ -12,27 +12,29 @@ import java.util.List;
 public class Volunteer {
     @Id
     @GeneratedValue
-    private Long vId;          // 봉사자 아이디
+    private Long vId;           // 봉사자 아이디
 
 //    @OneToMany(mappedBy = "volunteer", fetch = FetchType.EAGER) // proxy 객체가 담겨서 반환되지 않도록 Eager로 설정
 //    private List<ClassRoom> classRooms = new ArrayList<ClassRoom>();
 
     private String kakaoId;     // 카카오 회원번호
-    private String nickname;      // 별명
-    private int ageRange;        // 출생년도
-    private String introduce; // 자기소개
+    private String nickname;    // 별명
+    private int ageRange;       // 출생년도
+    private String introduce;   // 자기소개
+    private boolean talkable;   // 대화 가능 여부
 
     @Embedded
-    private Character character;//개인 3d캐릭터에 대한 정보
+    private Character character; //개인 3d캐릭터에 대한 정보
 
     @Builder
-    public Volunteer(Long vId, String kakaoId, String nickname,
-                     int ageRange, String introduce, Character character){
+    public Volunteer(Long vId, String kakaoId, String nickname, int ageRange,
+                     String introduce, boolean talkable, Character character){
         this.vId = vId;
         this.nickname = nickname;
         this.kakaoId = kakaoId;
         this.ageRange = ageRange;
         this.introduce = introduce;
+        this.talkable = talkable;
         this.character = character;
     }
 }
