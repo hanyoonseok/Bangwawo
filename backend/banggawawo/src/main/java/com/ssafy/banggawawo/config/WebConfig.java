aecf31f20177ececa62e8d0ee49664dcd4407ea3
정수빈 <integerbin2@naver.com>
2022-08-10 01:53:25 +0900

    [BE] feat : #53 이미지 가져오는 기능 추가

--- backend/banggawawo/src/main/java/com/ssafy/banggawawo/config/WebConfig.java
+++ backend/banggawawo/src/main/java/com/ssafy/banggawawo/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.banggawawo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,14 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedOriginPatterns("*")
                 .maxAge(3000);
-    }
-
-    //class_thumbnail을 위한 resource 접근허용
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/class_thumbnail/**")
-                .addResourceLocations("classpath:/class_thumbnail/")
-                .setCachePeriod(20);
     }
 }
