d6ac9b64a0235a7337c6a5f9e518491d97904e00
Lee Hwayeon <snw4002@hanmail.net>
2022-08-14 00:25:44 +0900

    [FE] feat : #60 학생이 오픈방 입장 시 자동 신청+세션 입장처리

--- frontend/src/views/class/ClassDetailView.vue
+++ frontend/src/views/class/ClassDetailView.vue
@@ -106,15 +106,23 @@
               </button>
             </div>
             <div v-else-if="userInfo.userType === 'student'">
-              <!-- 수업 신청을 했고 수업 시작한 경우 or 오픈방이면서 정원안찬방 -->
+              <!-- 비공개방이고 수업 신청을 했고 수업 시작한 경우 -->
               <button
                 v-if="
-                  (user.subscribe && classInfo.state === 1) ||
-                  (classInfo.opened && classInfo.enrolcnt < classInfo.maxcnt)
+                  user.subscribe && classInfo.state === 1 && !classInfo.opened
                 "
                 class="class-entrance-btn"
                 id="ing"
                 @click="entranceClass"
+              >
+                수업 입장
+              </button>
+              <!-- 오픈방이면서 정원안찬방 -->
+              <button
+                v-if="classInfo.opened && classInfo.enrolcnt < classInfo.maxcnt"
+                class="class-entrance-btn"
+                id="ing"
+                @click="enrolClass"
               >
                 수업 입장
               </button>
@@ -128,40 +136,46 @@
               >
                 입장 불가
               </button>
-              <!-- 수업 신청을 했고 수업 시작 안 한 경우 -->
+              <!-- 비공개방이고 수업 신청을 했고 수업 시작 안 한 경우 -->
               <button
-                v-else-if="user.subscribe && classInfo.state === 0"
+                v-else-if="
+                  user.subscribe && classInfo.state === 0 && !classInfo.opened
+                "
                 class="class-entrance-btn"
               >
                 수업 대기
               </button>
-              <!-- 수업 신청을 안했고 수업 시작 안했는데 정원 다 안찬경우 -->
+              <!-- 비공개방이고 수업 신청을 안했고 수업 시작 안했는데 정원 다 안찬경우 -->
               <button
                 v-else-if="
                   !user.subscribe &&
                   classInfo.state === 0 &&
-                  classInfo.enrolcnt < classInfo.maxcnt
+                  classInfo.enrolcnt < classInfo.maxcnt &&
+                  !classInfo.opened
                 "
                 class="class-subscribe-btn"
                 @click="enrolClass"
               >
                 수업 신청
               </button>
-              <!-- 수업 신청을 안했고 수업 시작 안했는데 정원이 차있는 경우 -->
+              <!-- 비공개방이고 수업 신청을 안했고 수업 시작 안했는데 정원이 차있는 경우 -->
               <button
                 v-else-if="
                   !user.subscribe &&
                   classInfo.state === 0 &&
-                  classInfo.enrolcnt >= classInfo.maxcnt
+                  classInfo.enrolcnt >= classInfo.maxcnt &&
+                  !classInfo.opened
                 "
                 class="class-subscribe-btn"
                 id="end"
               >
                 수업 신청불가
               </button>
-              <!-- 수업 신청을 안했고 수업 시작 한 경우 -->
+              <!-- 비공개방이고 수업 신청을 안했고 수업 시작 한 경우 -->
               <button
-                v-else-if="!user.subscribe && classInfo.state === 1"
+                v-else-if="
+                  !user.subscribe && classInfo.state === 1 && !classInfo.opened
+                "
                 class="class-subscribe-btn"
                 id="end"
               >
@@ -285,6 +299,11 @@ export default {
           console.log(response);
           user.subscribe = true;
           console.log("신청했니??", user.subscribe ? "ㅇㅇ" : "ㄴㄴ");
+          console.log("오픈방인가?", classInfo.value.opened);
+          if (classInfo.value.opened) {
+            //오픈방이면 신청수 올라가면서 바로 입장하게
+            entranceClass();
+          }
         })
         .catch((error) => {
           error;
@@ -365,8 +384,10 @@ export default {
     };
 
     //학생이 수업 입장
-    const entranceClass = () => {
-      store
+    const entranceClass = async () => {
+      console.log("userInfo.sid", userInfo.sid);
+      console.log("cid", cid);
+      await store
         .dispatch("root/entranceClass", { sid: userInfo.sid, cid: cid })
         .then((response) => {
           sessionId = response.data;
