3359e22c17578fe9fe45154bbef6ecd53c741774
육다빈 <yukdabin@naver.com>
2022-08-13 16:39:00 +0900

    [BE] feat : #63 감정 피드백 swagger 설명 추가

--- backend/banggawawo/src/main/java/com/ssafy/banggawawo/controller/EnrolController.java
+++ backend/banggawawo/src/main/java/com/ssafy/banggawawo/controller/EnrolController.java
@@ -46,7 +46,7 @@ public class EnrolController {
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-    @ApiOperation("수업에 수강신청한 학생 받아오기")
+    @ApiOperation(value = "수업에 수강신청한 학생 받아오기", notes="수업id(cid)를 입력받아 수업에 참여한 학생 목록을 반환한다")
     @GetMapping("/class/{cid}")
     public ResponseEntity<List<StudentFrontDto>> findByClassRoom(@PathVariable("cid") Long cId) throws Exception{
         List<Enrol> enrolList = enrolService.findByClassRoom(cId);
@@ -64,7 +64,7 @@ public class EnrolController {
     }
 
 
-    @ApiOperation("봉사자 피드백 작성")
+    @ApiOperation(value = "봉사자 피드백 작성" , notes="요청 값 : 클래스id(cid), 학생id(sid), 봉사자 피드백(feedback) \n반환 값 : 봉사자 피드백이 수정완료된 수강신청 정보(enrol)")
     @PutMapping("/feedback")
     public ResponseEntity<?> updateFeedBack(@RequestBody Map<String, Object> request) throws Exception{
         Long cid = Long.parseLong(request.get("cid").toString());
@@ -79,7 +79,7 @@ public class EnrolController {
             return new ResponseEntity<>(enrolService.findByClassAndStudent(cid, sid), HttpStatus.OK);
     }
 
-    @ApiOperation("감정 피드백 저장")
+    @ApiOperation(value = "감정 피드백 저장" , notes="요청 값 : 클래스id(cid), 학생id(sid), 감정피드백(emotion) \n반환 값 : 감정 피드백이 수정완료된 수강신청 정보(enrol)")
     @PutMapping("/emotion")
     public ResponseEntity<?> updateEmotion(@RequestBody Map<String, Object> request) throws Exception{
         ObjectMapper mapper = new ObjectMapper();
