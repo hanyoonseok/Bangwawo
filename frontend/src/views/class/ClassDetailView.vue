c6a32750ee3ac1fe98338654472a30caaaf92584
Lee Hwayeon <snw4002@hanmail.net>
2022-08-14 17:05:44 +0900

    [FE] feat : #60 수정

--- frontend/src/views/class/ClassDetailView.vue
+++ frontend/src/views/class/ClassDetailView.vue
@@ -254,6 +254,7 @@ export default {
           console.log(response.data);
           classInfo.value = response.data;
           console.log("수업 상태", classInfo.value.state);
+          console.log(classInfo.value);
         })
         .catch((error) => {
           console.log(error);
@@ -375,7 +376,7 @@ export default {
               nickname: userInfo.nickname,
               cid: cid,
               vid: userInfo.vid,
-              volunteerNickname: classInfo.vid.nickname,
+              volunteerNickname: classInfo.value.vid.nickname,
             },
           });
         })
@@ -400,7 +401,7 @@ export default {
               userType: userInfo.userType,
               sid: userInfo.sid,
               cid: cid,
-              volunteerNickname: classInfo.vid.nickname,
+              volunteerNickname: classInfo.value.vid.nickname,
             },
           });
         })
