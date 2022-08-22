a74ba5d72301bca8be7b83119273a5513c24ce36
한윤석 <hys6078@naver.com>
2022-08-23 00:42:14 +0900

    [FE] fix : add setting files

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/css/variabled.scss";
+        `,
+      },
+    },
+  },
+};
